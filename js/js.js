42cb796d09e690ed0a389d7cc96cb578e6616a3f
Wanderson A. Timoteo <wanderson_timoteo@hotmail.com>
2022-02-24 12:12:53 -0400

    Added JS function and styling for new divs created after clicking start game

---
+++ js/js.js
@@ -0,0 +1,10 @@
+function start() { // Start of the start() function
+
+	$("#start").hide();
+	
+	$("#game-background").append("<div id='player'></div>");
+	$("#game-background").append("<div id='enemy1'></div>");
+	$("#game-background").append("<div id='enemy2'></div>");
+	$("#game-background").append("<div id='friend'></div>");
+
+} // End of start function
