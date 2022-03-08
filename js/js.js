f87ad0f0215354d7e322a546146441897dabad3b
Wanderson A. Timoteo <wanderson_timoteo@hotmail.com>
2022-03-07 20:35:34 -0400

    Added movement in game background image

--- js/js.js
+++ js/js.js
@@ -7,4 +7,29 @@ function start() { // Start of the start() function
 	$("#game-background").append("<div id='enemy2'></div>");
 	$("#game-background").append("<div id='friend' class='animation3'></div>");
 
+	//Main game variables
+	
+	var game = {}
+	
+	//Game Loop
+
+	game.timer = setInterval(loop,30);
+	
+	function loop() {
+	
+	moveBackground();
+	
+	} // End of loop() function
+
+	
+//Function that moves the game background
+	
+function moveBackground() {
+	
+	left = parseInt($("#game-background").css("background-position"));
+	$("#game-background").css("background-position",left-1);
+	
+	} // end of moveBackground() function
+
+
 } // End of start function
