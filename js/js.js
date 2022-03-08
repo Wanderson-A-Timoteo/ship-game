308e5fd9402963f5e3818d17de5ab96c73ce256a
Wanderson A. Timoteo <wanderson_timoteo@hotmail.com>
2022-03-07 21:51:00 -0400

    Added up and down movement on the helicopter

--- js/js.js
+++ js/js.js
@@ -9,7 +9,27 @@ function start() { // Start of the start() function
 
 	//Main game variables
 	
-	var game = {}
+	var game = {};
+	var keyboardKey = {
+		upArrow: 38,
+		downArrow: 40,
+		backspace: 8
+		}
+	
+		game.pressed = [];
+
+	
+	//Checks if the user pressed any key
+	
+	//pressed=yes
+	$(document).keydown(function(e){
+		game.pressed[e.which] = true;
+		});
+	
+		//pressed=no
+		$(document).keyup(function(e){
+			game.pressed[e.which] = false;
+		});
 	
 	//Game Loop
 
@@ -18,6 +38,7 @@ function start() { // Start of the start() function
 	function loop() {
 	
 	moveBackground();
+	movePlayer();
 	
 	} // End of loop() function
 
@@ -31,5 +52,68 @@ function moveBackground() {
 	
 	} // end of moveBackground() function
 
+	function movePlayer() {
+	
+		if (game.pressed[keyboardKey.upArrow]) {
+			var TOP = parseInt($("#player").css("top"));
+			$("#player").css("top",TOP-10);
+	
+			if (TOP<=0) {
+			
+				$("#player").css("top",TOP+10);
+			}
+		
+		}
+		
+		if (game.pressed[keyboardKey.downArrow]) {
+			
+			var TOP = parseInt($("#player").css("top"));
+			$("#player").css("top",TOP+10);
+			
+			if (TOP>=434) {	
+				$("#player").css("top",TOP-10);
+					
+			}
+		}
+		
+		if (game.pressed[keyboardKey.backspace]) {
+			
+			//Call trigger function
+		}
+	
+		} // End of movePlayer() function
+
 
 } // End of start function
+
+
+function movePlayer() {
+	
+	if (game.pressed[keyboardKey.upArrow]) {
+		var topo = parseInt($("#player").css("top"));
+		$("#player").css("top",topo-10);
+
+		if (topo<=0) {
+		
+			$("#player").css("top",topo+10);
+		}
+	
+	}
+	
+	if (game.pressed[keyboardKey.downArrow]) {
+		
+		var topo = parseInt($("#player").css("top"));
+		$("#player").css("top",topo+10);
+		
+		if (topo>=434) {	
+			$("#player").css("top",topo-10);
+				
+		}
+	}
+	
+	if (game.pressed[keyboardKey.backspace]) {
+		
+		//Call trigger function
+	}
+
+	} // End of movePlayer() function
