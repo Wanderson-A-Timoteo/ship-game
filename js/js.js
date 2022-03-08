355ae8e9e21361fc41b6b59df55bf8fea23902c5
Wanderson A. Timoteo <wanderson_timoteo@hotmail.com>
2022-03-07 20:19:02 -0400

    Added helicopter animation

--- js/js.js
+++ js/js.js
@@ -2,8 +2,8 @@ function start() { // Start of the start() function
 
 	$("#start").hide();
 	
-	$("#game-background").append("<div id='player'></div>");
-	$("#game-background").append("<div id='enemy1'></div>");
+	$("#game-background").append("<div id='player' class='animation1'></div>");
+	$("#game-background").append("<div id='enemy1' class='animation2'></div>");
 	$("#game-background").append("<div id='enemy2'></div>");
 	$("#game-background").append("<div id='friend'></div>");
 
